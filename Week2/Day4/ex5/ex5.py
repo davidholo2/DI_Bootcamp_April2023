0909b34f04746d905657e27bbf7ade0bd4a31536
davidholo2 <davidholo2@gmail.com>
2023-05-09 22:04:34 +0300

    day5 mini_project

---
+++ Week2/Day4/ex5/ex5.py
@@ -0,0 +1,6 @@
+def make_shirt(size="large",text="i love python"):
+    print(f"The size of the shirt is {size} and the text is {text}")
+
+make_shirt()
+make_shirt("medium")
+make_shirt("small","no soup for you")
