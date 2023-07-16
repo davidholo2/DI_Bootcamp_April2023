f4d549f69b5c4c3736cfbec9dba37c81705e9ae6
davidholo2 <davidholo2@gmail.com>
2023-07-16 14:06:03 +0300

    w9 d3 ex

---
+++ Week9/Day3/ex/ex2.js
@@ -0,0 +1,16 @@
+const apiUrl =
+  "https://api.giphy.com/v1/gifs/search?q=sun&rating=g&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My&limit=10&offset=2";
+
+fetch(apiUrl)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error("Error fetching data:", error);
+  });
