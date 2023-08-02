42bac47d94b12565a7280aeede3eed719e54b6e9
davidholo2 <davidholo2@gmail.com>
2023-08-02 23:10:45 +0300

    w11 d3 dc

---
+++ Week11/Day3/dc/my-react-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Master from './components/form';
+
+function App() {
+  return (
+    <div className="App">
+      <Master />
+    </div>
+  );
+}
+
+export default App;
